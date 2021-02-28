a6de2a9f645a6e2b6faec3a551cc6979a8094e83
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2021-02-28 16:04:47 +0100

    Update adjacent_node.js

--- Challanges/Adjacente_Nodes/adjacent_node.js
+++ Challanges/Adjacente_Nodes/adjacent_node.js
@@ -1,7 +1,18 @@
+var matrix = [
+  [ 0, 1, 0, 0 ],
+  [ 1, 0, 1, 1 ],
+  [ 0, 1, 0, 1 ],
+  [ 0, 1, 1, 0 ]
+];
+
+var node1 = 0;
+var node2 = 1;
+
+
 const adjacent_nodes = (matrix, node1, node2) => {
-    for(let i = 0; x < matrix.length; x++) {
+    for(let x = 0; x < matrix.length; x++) {
       const row = matrix[node1];
-      for(let j = 0; y < row.length; y++) {
+      for(let y = 0; y < row.length; y++) {
         if(row[node2] == 0) {
           return false;
         } else {
@@ -9,6 +20,6 @@ const adjacent_nodes = (matrix, node1, node2) => {
         }
       }
     } 
-  }
+  };
   
-module.exports = adjacent_nodes
+console.log(adjacent_nodes(matrix, node1, node2));
