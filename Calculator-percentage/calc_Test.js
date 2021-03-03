26ff542c684b106a1e982fc77c203a1104a0d941
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2021-03-03 20:04:48 +0100

    Add files via upload

---
+++ Calculator-percentage/calc_Test.js
@@ -0,0 +1,7 @@
+var numField1 = document.getElementById('numField1');
+var numField2 = document.getElementById('numField2');
+var resultField = document.getElementById('resultField');
+
+
+numField1.value = "TEST";
+resultField.innerText = "TEST 2"
