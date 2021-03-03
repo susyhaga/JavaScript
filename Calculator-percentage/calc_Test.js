843d19f98461fb544f06be6c0ece74d56dbe58e0
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2021-03-03 20:40:38 +0100

    Delete calc_Test.js

--- Calculator-percentage/calc_Test.js
+++
@@ -1,7 +0,0 @@
-var numField1 = document.getElementById('numField1');
-var numField2 = document.getElementById('numField2');
-var resultField = document.getElementById('resultField');
-
-
-numField1.value = "TEST";
-resultField.innerText = "TEST 2"
