07c75bbb3109dd2ef42fdc88313273545a1ee155
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2021-05-25 12:44:58 +0200

    Add files via upload

---
+++ Testing/No tool/index.test.js
@@ -0,0 +1,12 @@
+//Testing without tool
+
+const { sum } = require("./lib")
+
+let result = sum(5, 10)
+
+if( result == 15 ) {
+  console.log("Test Summing works")
+}
+else {
+  console.error("Test Summing failed")
+}
